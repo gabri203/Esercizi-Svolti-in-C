b13f55f50bdc76ba1d503163e656673c8cd27a72
Said Gabriel <163392959+gabri203@users.noreply.github.com>
2024-03-17 20:59:33 +0100

    Add files via upload

---
+++ Triangolo_con_gli_asterischi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define N 4
+int main(){
+	int i,j,vettore[N];
+	
+    for(i=0;i<N;i++){
+    	
+    	scanf("%d",&vettore[i]);
+	}
+	for(i=0;i<vettore[i];i++){
+		for(j=0;j<vettore[i];j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+}
